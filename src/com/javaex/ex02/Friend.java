eafa970d9156c1d05dc1742540cf43eb89273a1d
가은 <rkdms8228@naver.com>
2022-05-03 16:45:59 +0900

    ex02

--- src/com/javaex/ex02/Friend.java
+++ src/com/javaex/ex02/Friend.java
@@ -2,10 +2,49 @@ package com.javaex.ex02;
 
 public class Friend {
 
-    private String name;
-    private String hp;
-    private String school;
-
-
+	//필드
+	private String name;
+	private String ph;
+	private String school;
+	
+	//생성자
+	public Friend() {
+	}
+	
+	public Friend(String name, String ph, String school) {
+		this.name = name;
+		this.ph = ph;
+		this.school = school;
+	}
+	
+	//메소드-gs
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPh() {
+		return ph;
+	}
+	public void setPh(String ph) {
+		this.ph = ph;
+	}
+	public String getSchool() {
+		return school;
+	}
+	public void setSchool(String school) {
+		this.school = school;
+	}
+	
+	//메소드-일반
+	@Override
+	public String toString() {
+		return "Friend [name=" + name + ", ph=" + ph + ", school=" + school + "]";
+	}
+	
+	public void showInfo() {
+		System.out.println("이름: "+name+ "   핸드폰: " + ph + "   학교: " + school);
+	}
 
 }
--- src/com/javaex/ex02/FriendApp.java
+++ src/com/javaex/ex02/FriendApp.java
@@ -1,13 +1,28 @@
 package com.javaex.ex02;
 
+import java.util.Scanner;
 
 public class FriendApp {
 
     public static void main(String[] args) {
 
-
+    	Friend[] ArrayList = new Friend[3];
     	
+    	Scanner sc = new Scanner(System.in);
     	
+    	for(int i =0; i<ArrayList.length; i++) {
+    		
+    		String[] info = sc.nextLine().split(" ");
+    		ArrayList[i] = new Friend(info[0], info[1], info[2]);
+    		
+    	}
+    	
+    	for(int i =0; i<ArrayList.length; i++) {
+    		ArrayList[i].showInfo();
+    	}
+    	
+    	sc.close();
+    
     }
 
 }
