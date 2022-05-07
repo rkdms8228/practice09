49bf7bac29c78f12ba02217ce0de49672c56b0d8
가은 <rkdms8228@naver.com>
2022-05-07 21:35:13 +0900

    띄어쓰기 수정

--- src/com/javaex/ex04/RectTriangle.java
+++ src/com/javaex/ex04/RectTriangle.java
@@ -1,19 +1,43 @@
 package com.javaex.ex04;
 
 public class RectTriangle extends Shape {
+	
+	//필드
     private double width;
     private double height;
 
+    //생성자
+    public RectTriangle() {
+    }
+    
     public RectTriangle( double width, double height ) {
         this.width = width;
         this.height = height;
     }
     
+    //메소드-일반
+    public double getWidth() {
+		return width;
+	}
+
+	public void setWidth(double width) {
+		this.width = width;
+	}
+
+	public double getHeight() {
+		return height;
+	}
+
+	public void setHeight(double height) {
+		this.height = height;
+	}
+    
+    //메소드-일반
     public double getArea() {
         return width*height*0.5;
     }
 
-    public double getPerimeter() {
+	public double getPerimeter() {
         double result = width + height + Math.sqrt( width*width + height*height );
         return result;
             
--- src/com/javaex/ex04/Rectangle.java
+++ src/com/javaex/ex04/Rectangle.java
@@ -1,22 +1,46 @@
 package com.javaex.ex04;
 
 public class Rectangle extends Shape implements Resizeable {
+	
+	//필드
     private double width;
     private double height;
 
+    //생성자
+    public Rectangle() {
+    }
+    
     public Rectangle( double width, double height ) {
         this.width = width;
         this.height = height;
     }
     
-    public double getArea() {
+    //메소드-gs
+    public double getWidth() {
+		return width;
+	}
+
+	public void setWidth(double width) {
+		this.width = width;
+	}
+
+	public double getHeight() {
+		return height;
+	}
+
+	public void setHeight(double height) {
+		this.height = height;
+	}
+    
+    //메소드-일반
+	public double getArea() {
         return width * height;
     }
-
-    public double getPerimeter() {
+	
+	public double getPerimeter() {
         return 2*(width + height);
     }
-
+    
     public void resize(double s) {
         width = width * s;
         height = height * s;
